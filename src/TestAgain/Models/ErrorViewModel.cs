6954e42f6cbd056f206a19cd4b9974277e4d3ded
Rajah Iyer <codeaiplings@gmail.com>
2023-07-02 13:58:19 +0200

    Add folder for connections changes

---
+++ src/TestAgain/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestAgain.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

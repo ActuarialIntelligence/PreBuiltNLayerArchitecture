cb979ca0b39c3c78a58a22e3728d74dd972ab770
Rajah Iyer <codeaiplings@gmail.com>
2023-07-02 13:45:11 +0200

    Clean Commit

--- src/AML.Serialization/FICOFileSerializer.cs
+++ src/AML.Serialization/FICOFileSerializer.cs
@@ -1,11 +1,7 @@
 ﻿using SKF.Domain;
 using SKF.Infrastructure.Readers.Interfaces;
 using SKF.Serialization.Interfaces;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SKF.Serialization
 {
